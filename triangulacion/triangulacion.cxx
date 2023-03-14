dac88db37a27cefe0836daa9876b0f8a115e1103
Dhasane <danihas@live.com>
2023-03-14 01:50:57 +0000

    test

--- triangulacion/triangulacion.cxx
+++ triangulacion/triangulacion.cxx
@@ -1,7 +1,7 @@
 
 #include <iostream>
 #include <vector>
-#include <vector>
+#include <stack>
 
 #include <CGAL/Cartesian.h>
 #include <CGAL/partition_2.h>
@@ -26,6 +26,154 @@ struct V3 {
     T v3;
 };
 
+int calidad_triangulo(TPoint2 p1, TPoint2 p2, TPoint2 p3)
+{
+
+}
+
+bool right_segment(TPoint2 origin, TPoint2 punto)
+{
+    return origin.x() < punto.x();
+}
+
+struct PointOrder
+{
+    int posicion;
+    bool right;
+};
+struct Par
+{
+    int posicionIni;
+    int posicionFin;
+};
+
+void triangulacion(std::deque<TPoint2> P)
+{
+    std::deque<Par> D;
+    std::deque<PointOrder> S;
+
+    std::sort(P.begin(), P.end(), [](TPoint2 a, TPoint2 b)
+        {
+            if (a.y() < b.y())
+            {
+                return 1;
+            }
+            else if (a.y() > b.y())
+            {
+                return -1;
+            }
+
+            if (a.x() < b.x())
+            {
+                return 1;
+            }
+            else if (a.x() > b.x())
+            {
+                return -1;
+            }
+
+            return 0;
+        });
+
+    TPoint2 origin = P.front();
+
+    S.push_front({0, false});
+    S.push_front({1, right_segment(origin, P[1])});
+
+    for (int i=2; i<P.size() ; i++)
+    {
+        TPoint2 Uj = P[i];
+        bool right = right_segment(origin, Uj);
+        bool prev_right = S.front().right;
+
+        int f_s_size = 0;
+
+        PointOrder last;
+
+        PointOrder current{i, right};
+
+        if (right != prev_right)
+        {
+            last = S.front();
+            S.pop_front();
+
+        }
+        else
+        {
+            f_s_size = 1;
+            // se puede usar greene para verificar si se sale
+            // positivo es interno, negativo es externo
+        }
+
+        while( f_s_size >= S.size())
+        {
+            D.push_back({i, S.front().posicion});
+            S.pop_front();
+        }
+    }
+}
+
+int next(int act, int inc, int max)
+{
+    int ch = (act + inc) % max;
+    return ch == -1 ? max - 1 : ch;
+}
+
+void triangulacion_rot(std::deque<TPoint2> P)
+{
+    std::deque<Par> D;
+    std::deque<PointOrder> S;
+
+    std::sort(P.begin(), P.end(), );
+
+    auto comp = [](TPoint2 a, TPoint2 b)
+        {
+            if (a.y() < b.y())
+            {
+                return 1;
+            }
+            else if (a.y() > b.y())
+            {
+                return -1;
+            }
+
+            if (a.x() < b.x())
+            {
+                return 1;
+            }
+            else if (a.x() > b.x())
+            {
+                return -1;
+            }
+
+            return 0;
+        };
+
+    // ordenamiento
+
+    int p_ant;
+    int p_act = -1 ;
+    int p_nxt;
+
+    int inc = -1;
+    if (P[0] < P[1])
+    {
+        inc = 1;
+    }
+
+    do
+    {
+        p_ant = next(p_act, -inc, P.size());
+        p_act = next(p_act, inc, P.size());
+        p_nxt = next(p_act, inc, P.size());
+    } while (P[p_ant] < P[p_act] && P[p_act] < P[p_nxt]);
+
+    // final ordenamiento
+
+    std::cout << p_act << std::endl;
+
+}
+
 // template <typename T>
 // void triangulacion_monotona(typename std::vector<T>::iterator start,
 //                             typename std::vector<T>::iterator end,
@@ -45,6 +193,9 @@ struct V3 {
 //             triangulos.push_back({*start, *end, *end++});
 //     }
 // }
+
+
+
 
 int main() {
     // std::vector<_K::Point_2 > points =
@@ -89,6 +240,17 @@ int main() {
         std::back_inserter( partition ), traits
         );
 
+    // std::list< _T::Polygon_2 > y_partition;
+    // CGAL::y_monotone_partition_2(
+    //     polygon.vertices_begin( ), polygon.vertices_end( ),
+    //     std::back_inserter( y_partition ), traits
+    //     );
+
+    // CGAL::monotone_partition_2(
+    //     y_partition.vertices_begin( ), y_partition.vertices_end( ),
+    //     std::back_inserter( partition ), traits
+    //     );
+
     std::cout << "# Vertices" << std::endl;
     for( const auto& p: points )
         std::cout << "v " << p << " 0" << std::endl;
@@ -96,7 +258,6 @@ int main() {
     std::cout << std::endl;
 
     std::cout << "# Lines" << std::endl;
-    std::vector<V3<int>> triangulos;
     for( const auto& poly: partition )
     {
         auto container = poly.container( );
