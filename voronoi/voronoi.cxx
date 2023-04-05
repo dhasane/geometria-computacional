fdc0a185e222fc44316167b67699175d956794cd
Dhasane <danihas@live.com>
2023-04-05 02:16:38 +0000

    calcular tamaño caras

--- voronoi/voronoi.cxx
+++ voronoi/voronoi.cxx
@@ -11,17 +11,14 @@
 #include <CGAL/Delaunay_triangulation_adaptation_policies_2.h>
 #include <CGAL/draw_triangulation_2.h>
 
-// esto es un alias
+using K  = CGAL::Exact_predicates_inexact_constructions_kernel;
+using DT = CGAL::Delaunay_triangulation_2<K>;
+using AT = CGAL::Delaunay_triangulation_adaptation_traits_2<DT>;
+using AP = CGAL::Delaunay_triangulation_caching_degeneracy_removal_policy_2<DT>;
+using VD = CGAL::Voronoi_diagram_2<DT,AT,AP>;
 
-// typedefs for defining the adaptor
-typedef CGAL::Exact_predicates_inexact_constructions_kernel                  K;
-typedef CGAL::Delaunay_triangulation_2<K>                                    DT;
-typedef CGAL::Delaunay_triangulation_adaptation_traits_2<DT>                 AT;
-typedef CGAL::Delaunay_triangulation_caching_degeneracy_removal_policy_2<DT> AP;
-typedef CGAL::Voronoi_diagram_2<DT,AT,AP>                                    VD;
-
-using Face   = VD::Face;
-using Vertex = VD::Vertex;
+using Face     = VD::Face;
+using Vertex   = VD::Vertex;
 using Halfedge = VD::Halfedge;
 
 using TPoint2 = K::Point_2;
@@ -47,51 +44,45 @@ double area_green(std::vector<TPoint2>::iterator start, std::vector<TPoint2>::it
     return area / 2;
 }
 
-void process_faces(VD::Face_iterator faces_begin, VD::Face_iterator faces_end) {
+double calculate_face_area (Face &a) {
+    std::vector<TPoint2> pts;
+    Halfedge he = *a.halfedge();
 
-    std::map<Face*, double> areas;
+    bool check_area = true;
+    while(he.next() != a.halfedge())
+    {
+        // siempre poner el punto anterior
+        if (he.previous()->has_target()) {
+            pts.push_back(he.previous()->target()->point());
+            he = *he.next();
+        } else {
+            check_area = false;
+            break;
+        }
+    }
+    if (check_area) {
+        return area_green(pts.begin(), pts.end());
+    }
+    return 0;
+};
 
-    auto calculate_face_area = [](Face &a) {
-		std::vector<TPoint2> pts;
-		Halfedge he = *a.halfedge();
+int get_faces_areas(VD::Face_iterator faces_begin, VD::Face_iterator faces_end) {
+    int pos_max = 0;
+    double max_area = 0;
 
-		while(he.next() != a.halfedge())
-		{
-			// siempre poner el punto anterior
-			pts.push_back(he.previous()->target()->point());
-			he = *he.next();
-		}
-
-		return area_green(pts.begin(), pts.end());
-	};
-
-    auto get_area = [&areas, &calculate_face_area](Face &a) {
-		if (areas.find(&a) == areas.end()) {
-			double area = calculate_face_area(a);
-			areas[&a] = area;
-		}
-		return areas[&a];
-	};
-
-    auto face_area_comp = [&get_area](Face a, Face b) {
-		return get_area(a) > get_area(b);
-	};
-
+    int pos = 0;
     for (auto it = faces_begin; it != faces_end; it++) {
-		get_area(*it);
-	}
+        double area = calculate_face_area(*it);
+        std::cout << pos << ": " << area << std::endl;
+        if (max_area < area) {
+            max_area = area;
+            pos_max = pos;
+        }
+        pos++;
+    }
 
-    // std::sort(faces_begin, faces_end, face_area_comp);
-
-	std::cout << "areas" << std::endl;
-	for (auto it = areas.begin(); it != areas.end(); it++)
-	{
-		std::cout << it->first    // string (key)
-				  << ':'
-				  << it->second   // string's value
-				  << std::endl;
-	}
-
+    std::cout << "max area: " << max_area << " pos: " << pos << std::endl;
+    return pos_max;
 }
 
 int main(int argc, char** argv)
@@ -108,9 +99,7 @@ int main(int argc, char** argv)
         vd.insert(TPoint2(dis(gen), dis(gen)));
     }
 
-	// CGAL::draw(vd);
-
-	process_faces(vd.faces_begin(), vd.faces_end());
+	int pos_max = get_faces_areas(vd.faces_begin(), vd.faces_end());
 
     return 0;
 }
