f3e73ecc67744638a723a9b03ee308d5b6f4ec18
Dhasane <danihas@live.com>
2023-05-01 05:54:27 +0000

    Casi que ya, pero no entiendo el problema
    en este momento se crea una cara vacia en ciertos casos, pero no
    entiendo la razon.

--- CGAL_curvatures/bash/MeshSphere.cxx
+++ CGAL_curvatures/bash/MeshSphere.cxx
@@ -16,43 +16,128 @@ using TKernel = CGAL::Cartesian< double >;
 using TReal = TKernel::FT;
 using TPoint = TKernel::Point_3;
 using TMesh = CGAL::Surface_mesh< TPoint >;
-using TFace = CGAL::Surface_mesh< TPoint >::face;
+using TFace = TMesh::Face_index;
 
 using TFunction = CGAL_EXT::Sphere_function< TKernel >;
 
 class Topologia {
+    std::map<int, TPoint> m;
+	TMesh *tm;
 
-    TMesh::face_index closest_face(TMesh tm, TMesh::vertex_index vertex) {
-        TMesh::face_iterator vb, ve;
-        for (boost::tie(vb, ve) = faces(tm); vb != ve; ++vb) {
-			// TODO esto aun no sirve
-			TFace face = tm.face(vb)
-            std::cout << "        " << tm.face(vb) << "\n";
-        }
-    }
 
-    void replace_face(TMesh tm, TMesh::face_index face, TMesh::vertex_index pt) {
-        CGAL::Vertex_around_face_iterator<TMesh> vbegin, vend;
-        boost::tie(vbegin, vend) = vertices_around_face(tm.halfedge(face), tm);
-        auto fit = vbegin++;
+public:
+	Topologia(TMesh *m) {
+		this->tm = m;
+	}
 
-        for (; fit != vend--; ++fit) {
-            tm.add_face(pt, *fit--, *fit);
-        }
+	TMesh::face_index add_face(TMesh::vertex_index p1,
+							   TMesh::vertex_index p2,
+							   TMesh::vertex_index p3) {
+		bool valid1 = tm->has_valid_index(p1);
+		bool valid2 = tm->has_valid_index(p2);
+		bool valid3 = tm->has_valid_index(p3);
+		std::cout << "adding: " << std::endl
+				  << "\t" << p3 << " is valid " << valid1 << std::endl
+				  << "\t" << p2 << " is valid " << valid2 << std::endl
+				  << "\t" << p1 << " is valid " << valid3 << std::endl;
+		TMesh::face_index f = tm->add_face(p1, p2, p3);
+		assert(f != TMesh::null_face());
+		return f;
+	}
 
-        tm.remove_face(face);
-    }
-
-    void get_topologia(TMesh tm, std::vector<TMesh::vertex_index> puntos) {
-        tm.add_face(puntos[0], puntos[1], puntos[2]);
-        tm.add_face(puntos[2], puntos[1], puntos[0]);
+    void get_topologia(std::vector<TMesh::vertex_index> puntos) {
+        add_face(puntos[2], puntos[1], puntos[0]);
+        add_face(puntos[0], puntos[1], puntos[2]);
 
         for (int a = 3; a < puntos.size(); a++) {
-            TMesh::face_index face = closest_face(tm, puntos[a]);
+			std::cout << "started with " << a << std::endl;
+            TMesh::face_index face = closest_face(puntos[a]);
 
-            replace_face(tm, face, puntos[a]);
+			std::cout << "closest face index " << face << std::endl;
+
+            replace_face(face, puntos[a]);
+			return;
         }
     }
+
+	void print_all_faces() {
+        TMesh::face_iterator vb, ve;
+		std::cout << " all faces : " << std::endl;
+        for (boost::tie(vb, ve) = faces(*tm); vb != ve; ++vb) {
+			TMesh::Halfedge_index hf = tm->halfedge(*vb);
+			TMesh::Face_index fi = tm->face(hf);
+			std::cout << fi << " ";
+        }
+		std::cout << std::endl;
+	}
+
+private:
+    TMesh::face_index closest_face(TMesh::vertex_index vertex_index) {
+        TMesh::face_iterator vb, ve;
+
+		TPoint vertex = tm->point(vertex_index);
+
+		double min_dist = 0;
+		TMesh::Face_index min_face_index;
+
+		bool first = true;
+
+        for (boost::tie(vb, ve) = faces(*tm); vb != ve; ++vb) {
+            // TMesh::Face_index face_index = tm.face(tm.halfedge(*vb));
+            // tm.face(face);
+
+			TMesh::Halfedge_index hf = tm->halfedge(*vb);
+
+			// en caso de que la cara aun no este presente en el mapa
+			// evitar calcular esto multiples veces
+			TMesh::Face_index fi = tm->face(hf);
+			if (m.count(fi) == 0) {
+				double x;
+				double y;
+				double z;
+				for (TMesh::Halfedge_index hi : halfedges_around_face(hf, *tm)) {
+					TMesh::Vertex_index vi = target(hi, *tm);
+					TPoint p = tm->point(vi);
+					x += p.hx();
+					y += p.hy();
+					z += p.hz();
+				}
+
+				m[fi] = TPoint{x,y,z};
+			}
+
+			double dist = squared_distance(m[fi], vertex);
+			if (first || dist <= min_dist) {
+				min_dist = dist;
+				min_face_index = fi;
+
+				first = false;
+			}
+        }
+		return min_face_index;
+    }
+
+    void replace_face(TMesh::face_index face, TMesh::vertex_index pt) {
+        CGAL::Vertex_around_face_iterator<TMesh> vbegin, vend;
+        boost::tie(vbegin, vend) = vertices_around_face(tm->halfedge(face), *tm);
+        auto fit = vbegin++;
+
+		std::cout << " pre add ";
+		print_all_faces();
+        for (; fit != vend; ++fit) {
+			// TODO aqui hay un error
+			add_face(pt, *std::next(fit, -1), *fit);
+        }
+
+                std::cout << " pre remove ";
+		print_all_faces();
+
+		m.erase(face);
+        tm->remove_face(face);
+		std::cout << "face removed from map " << face << std::endl;
+		print_all_faces();
+    }
+
 };
 
 // -------------------------------------------------------------------------
@@ -93,8 +178,9 @@ int main( int argc, char** argv )
   } // end for
 
   // Topology
+  Topologia t(&m);
 
-  // TODO aqui seria llamar lo de arriba
+  t.get_topologia(puntos);
 
   // Save results
   CGAL::IO::write_OBJ( "mesh.obj", m );
