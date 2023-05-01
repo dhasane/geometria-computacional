cc6c25d61e9beeef767529f4007235ec6f9d61a5
Dhasane <danihas@live.com>
2023-05-01 20:14:27 +0000

    Remove if

--- CGAL_curvatures/bash/MeshSphere.cxx
+++ CGAL_curvatures/bash/MeshSphere.cxx
@@ -118,28 +118,21 @@ private:
     }
 
     void replace_face(TMesh::face_index face, TMesh::vertex_index pt) {
-	typename TMesh::Vertex_around_face_iterator vbegin, vend;
-        //CGAL::Vertex_around_face_iterator<TMesh> vbegin, vend;
-        boost::tie(vbegin, vend) = tm.vertices_around_face(tm.halfedge(face));
-		//vertices_around_face(tm->halfedge(face), *tm);
-	    
-	    //no se si el orden de estas declaraciones afecte
-        	auto fit = vbegin++;
-		std::cout << " pre add ";
-		print_all_faces();
+		typename TMesh::Vertex_around_face_iterator vbegin, vend;
+        boost::tie(vbegin, vend) = tm->vertices_around_face(tm->halfedge(face));
+
+        auto fit = vbegin++;
+
+        std::cout << " pre add ";
+        print_all_faces();
         for (; fit != vend; ++fit) {
-		typename TMesh::Vertex_index prev_vertex;
-		if (fit == vbegin) {
-            	prev_vertex = *(vend - 1);
-      		  } else {
-         	   prev_vertex = *(fit - 1);
-      		  }
-       		 tm.add_face(pt, prev_vertex, *fit);
+        	typename TMesh::Vertex_index prev_vertex = *std::next(fit, -1);
+
 			// TODO aqui hay un error
-			////add_face(pt, *std::next(fit, -1), *fit);
+			add_face(pt, prev_vertex, *fit);
         }
 
-                std::cout << " pre remove ";
+		std::cout << " pre remove ";
 		print_all_faces();
 
 		//m.erase(face);
