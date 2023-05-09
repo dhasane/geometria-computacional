1e0b0595df39d1cc7d7d8ed9e3d644a1acc43b6a
Dhasane <danihas@live.com>
2023-05-09 00:06:04 -0500

    algo

--- simplex_mesh/simplex.cxx
+++ simplex_mesh/simplex.cxx
@@ -12,15 +12,58 @@ using TMesh = CGAL::Surface_mesh< TPoint >;
 using TPoint_ind = TMesh::vertex_index;
 using TFace = TMesh::Face_index;
 
-struct Circle {
+double dot_product(TPoint p1, TPoint p2) {
+  return p1.x() * p2.x() + p1.y() * p2.y() + p1.z() * p2.z();
+}
+
+struct Circ {
 	TPoint center;
 	double radius;
 };
 
-struct Vecinos {
+class Adjacents {
+	TPoint center;
+
 	TPoint v1;
 	TPoint v2;
 	TPoint v3;
+
+	Adjacents (TPoint cntr, TPoint v1, TPoint v2, TPoint v3) {
+		this->center = cntr;
+		this->v1 = v1;
+		this->v2 = v2;
+		this->v3 = v3;
+	}
+
+	double magnitude() {
+		return sqrt(
+			dot_product(v1, v1) +
+			dot_product(v2, v2) +
+			dot_product(v3, v3)
+			);
+	}
+
+	double normal_plano_tangente() {
+		double v = dot_product(v1, v2) + dot_product(v2, v3) + dot_product(v3, v1);
+		return v / magnitude();
+	}
+
+	Circ curcumscribed_circle() {
+		double x = (v1.x() + v2.x() + v3.x())/3;
+		double y = (v1.y() + v2.y() + v3.y())/3;
+		double z = (v1.z() + v2.z() + v3.z())/3;
+
+		TPoint triang_center{x, y, z};
+
+		return Circ{ triang_center, squared_distance(triang_center, v1) }; // S
+	}
+
+	Circ circumscribed_sphere() {
+		// TODO: no entiendo esto
+		sin = r / R;
+		cos;
+
+	}
 };
 
 class SimplexMesh {
@@ -36,22 +79,6 @@ class SimplexMesh {
 	}
 
 
-	Circle get_circ(TMesh::vertex_index point) {
-		Vecinos v = encontrar_vecinos(point);
-
-		double x = (v.v1.x() + v.v2.x() + v.v3.x())/3;
-		double y = (v.v1.y() + v.v2.y() + v.v3.y())/3;
-		double z = (v.v1.z() + v.v2.z() + v.v3.z())/3;
-
-		TPoint center{x, y, z};
-
-		return Circle{ center, squared_distance(center, v.v1) };
-	}
-
-	Circle get_sphere(TMesh::vertex_index point) {
-		Vecinos v = encontrar_vecinos(point);
-
-	}
 
 };
 
