ec3ce6b95231010959ea1cb8ca6f6a7dcbf505c5
Dhasane <danihas@live.com>
2023-05-17 14:10:10 -0500

    Ya es una esfera

--- laberinto/laberinto.cxx
+++ laberinto/laberinto.cxx
@@ -88,6 +88,13 @@ public:
 
 #define OUTSIDE {-2, -2, -2}
 
+float distancia(Pos p1, Pos p2) {
+	Pos dist{p1.x - p2.x, p1.y - p2.y, p1.z - p2.z};
+	return sqrt( dist.x * dist.x +
+				 dist.y * dist.y +
+				 dist.z * dist.z);
+}
+
 class Labyrinth {
 
 	Room ***rooms;
@@ -109,13 +116,15 @@ public:
 		// matrix cuadrada
 		rooms = new Room **[max.x];
 		std::vector<Pos> posible_inicio;
+
+		Pos centro {max.x/2,max.y/2,max.z/2};
 		for (int x = 0; x < max.x; ++x) {
 			rooms[x] = new Room *[max.y];
 			for (int y = 0; y < max.y; ++y) {
 				rooms[x][y] = new Room[max.z];
 				for (int z = 0 ; z < max.z ; z++)
 				{
-					if (x > y){
+					if (centro.x < distancia({x, y, z}, centro)){
 						rooms[x][y][z].fill();
 					}
 					if (z==0 && !rooms[x][y][z].is_filled()) {
